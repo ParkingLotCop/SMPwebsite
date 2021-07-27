36a0765c5ec0fa94ca3df3fdf14405c98b82890e
timothypidashev <pidashev.tim@gmail.com>
2021-07-27 12:02:12 -0700

    upload some more changes

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/templates")
 
-
-@app.route('/')
-def home():
-    return app.send_static_file("index.html")
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
