ebb03d9b05c3b84f2faf425a37d7d863377812e7
timothypidashev <pidashev.tim@gmail.com>
2021-07-27 19:45:46 -0700

    upload first major version

--- index.py
+++ index.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__, static_url_path="/templates")
+app = Flask(__name__, template_folder = "templates", static_url_path="/static")
 
+#home page
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/ranks")
+def ranks():
+    return render_template("ranks.html")
     
+#run app
+if __name__ == "__main__":
+    app.run(debug=True)
